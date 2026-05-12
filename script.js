4a3dfa3789719bc319b8afc8a84839ba5695eba2
Apollo67-aura <hendriel.brandao@escola.pr.gov.br>
2026-05-11 21:06:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function showPage(id){
+
+document.querySelectorAll('.page').forEach(page=>{
+page.classList.remove('active')
+})
+
+document.getElementById(id).classList.add('active')
+}
+
+function login(){
+alert('Login realizado com sucesso!')
+}
+
+function buyGame(name){
+alert('Pagamento simulado aprovado para: ' + name)
+}
